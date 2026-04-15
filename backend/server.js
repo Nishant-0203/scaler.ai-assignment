9d6cfe0d1f04134c6a1032684427a9b3f765cc8a
Nishant Bhalla <nishantbhalla32@gmail.com>
2026-04-15 11:54:45 +0530

    feat: update package.json scripts and conditionally start server in development

--- backend/server.js
+++ backend/server.js
@@ -71,9 +71,11 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
+}
 
 module.exports = app;
 
